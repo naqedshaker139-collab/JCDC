063342d0e760d8ebe4942a88f4b5840c57f96150
naqedshaker139-collab <naqedshaker139@gmail.com>
2025-11-10 14:34:21 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from main import app
+from src.main import app
+from src.seed_data import seed_data
+
+# --- ADD THIS CODE BLOCK ---
+with app.app_context():
+    seed_data()
+# ---------------------------
 
 if __name__ == "__main__":
     app.run()
