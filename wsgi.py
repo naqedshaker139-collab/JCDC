06960a56d11a86b5e5089754737f9aa695cc624f
naqedshaker139-collab <naqedshaker139@gmail.com>
2025-11-11 12:44:14 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,5 @@
 # In backend/wsgi.py
-
-from src.main import app
-from src.seed_data import seed_database # Import the function
-
-# --- ADDED: Call the seeding function before the app starts ---
-# This ensures the database is populated every time the service wakes up
-with app.app_context():
-    seed_database()
-# -------------------------------------------------------------
+from main import app
 
 if __name__ == "__main__":
     app.run()
